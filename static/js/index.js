1a46dece2dbd80b8b41a9c8f116571a6f4141306
Hongliang TIAN <tatetian@gmail.com>
2012-02-08 08:57:36 +0800

    Created "details of entry" page, still need improve

--- static/js/index.js
+++ static/js/index.js
@@ -11,13 +11,13 @@ var fm = {
         if (fm.is_slide_primary)
             $("#main.slider").height($("#main.slider > .slide.primary").height()); 
         else
-            $("#main.slider").height($("#main.slider > .slide.secondary").height()); 
+            $("#main.slider").height($("#main.slider > .slide.secondary").height()+4); 
     }
 };
 $(document).ready(function() {
     // Event handlers
     // Open/close top-panel
-    $("#topbar .slide.primary .title span").click(fm.toggleTopbar);
+    $("#topbar .slide.primary .title").click(fm.toggleTopbar);
     $("#top-panel .entries .entry").click(fm.toggleTopbar);
     // Expand search bar
     $("#search").focus(function(e){
@@ -29,6 +29,9 @@ $(document).ready(function() {
             fm.is_slide_primary = false;
             $(".slide.primary").animate({"left":"-100%"});
             $(".slide.secondary").animate({"left":"0%"});
+            $("#top-panel .main .tab.primary").hide();
+            $("#top-panel .bottombar").hide();
+            $("#top-panel .main .tab.secondary").show();
             fm.onWindowResize();    
         }
     });
@@ -37,6 +40,9 @@ $(document).ready(function() {
             fm.is_slide_primary = true;
             $(".slide.primary").animate({"left":"0%"});
             $(".slide.secondary").animate({"left":"100%"});
+            $("#top-panel .main .tab.primary").show();
+            $("#top-panel .bottombar").show();
+            $("#top-panel .main .tab.secondary").hide();
             fm.onWindowResize();
         } 
     }); 
