2e6e4a24c62e5da372c2c335a35d35d7e9d254e7
Hongliang TIAN <tatetian@gmail.com>
2012-02-19 23:58:20 +0800

    Add search JS

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,7 @@
 function FmManager() {
     this.topPanel = new FmTopPanel(this);
     this.mainPanel = new FmMainPanel(this);
-    //this.webService = FmWebService(this);
+    this.webService = new FmWebService(this);
 
     this.state = {
         isPrimaryView : true,
@@ -10,7 +10,8 @@ function FmManager() {
     };
     this.elements = {
         scrollOwner: $.browser.msie || $.browser.mozilla || $.browser.opera ? "html" : "body",
-        slides: ".slide"
+        slides: ".slide",
+        loader: "body > .loading"
     };
 }
 FmManager.prototype.init = function() {
@@ -27,7 +28,7 @@ FmManager.prototype.init = function() {
             $("#top-panel .bottombar").show();
             $("#top-panel .main .tab.secondary").hide();*/
             manager.scrollTo(manager.state.lastScrollTop);
-            manager.mainPanel.windowResize();
+            manager.mainPanel.updateHeight();
         } 
     });
     this.mainPanel.clickRightBtn({manager: this}, function(e) {
@@ -40,17 +41,33 @@ FmManager.prototype.init = function() {
 /*            $("#top-panel .main .tab.primary").hide();
             $("#top-panel .bottombar").hide();
             $("#top-panel .main .tab.secondary").show();*/
-            manager.mainPanel.windowResize();    
+            manager.mainPanel.updateHeight();    
         }
     }); 
+    // load data
+    this.search("all", null);
 }
 FmManager.prototype.showLoading = function() {
+    $(this.elements.loader).fadeIn();
 }
-
 FmManager.prototype.hideLoading = function() {
+    $(this.elements.loader).fadeOut();
 }
 FmManager.prototype.scrollTo = function(pos) {
      $(this.elements.scrollOwner).animate({"scrollTop": pos});
+}
+FmManager.prototype.search = function(tag, keywords) {
+    var context = {manager: this};
+    this.webService.docsSearch(tag, keywords, context, function(response, context) {
+        var manager = context.manager;
+        if(!response.error) {
+            manager.mainPanel.showResult(response.result);
+            manager.hideLoading();
+        }
+        else {  // handle error
+            
+        }
+    });
 }
 /*******************************FmTopPanel*******************************/
 function FmTopPanel(manager) {
@@ -102,16 +119,17 @@ function FmMainPanel(manager) {
         "me": "#main.slider",
         "primaryView": "#main.slider > .slide.primary",
         "secondaryView": "#main.slider > .slide.secondary",
-        "rightBtns": ".entry li.button.arrow-right-icon"
+        "rightBtns": ".entry li.button.arrow-right-icon",
+        "result": "#main .slide.primary .result"
     };
 }
 FmMainPanel.prototype.init = function() {
     $(window).resize({ mainPanel: this }, function(e) {
-        e.data.mainPanel.windowResize();
+        e.data.mainPanel.updateHeight();
     });
-    this.windowResize();
+    this.updateHeight();
 }
-FmMainPanel.prototype.windowResize = function() {
+FmMainPanel.prototype.updateHeight = function() {
     if (this.manager.state.isPrimaryView)
         $(this.elements.me).height($(this.elements.primaryView).height()); 
     else
@@ -124,37 +142,106 @@ FmMainPanel.prototype.clickRightBtn = function(data, callback) {
             data, 
             callback);
 }
-/* Web Services */
-ws = {
-    "docs": {
-        "search": 
-        /* Search relevant docs according to some conditions
-         * @arg tag string
-         * @arg keywords array
-         * @arg sort_key string
-         * @arg from
-         * @arg to 
-         * */
-        function() {
-
-        },
-        "upload": function() {
-        },
-        "delete": function() {},
-        "edit": function() {},
-        "detail": function() {}
-    },
-    "tags": {
-        "list": function() {},
-        "edit": function() {},
-        "add": function() {},
-    },
-    "tag": {
-        "add": function() {},
-        "edit": function() {},
-        "delete": function() {}
+FmMainPanel.prototype.showResult = function(result) {
+    var $result = $(this.elements.result);
+    // remove old result
+    $result.hide();
+    $result.children().remove();
+    // construct new result
+    // according to www.learningjquery.com/2009/03/43439-reasons-to-use-append-correct
+    // below is the best way to insert many HTML elements into DOM
+    var rightBtnHtml = '<ul class="buttons"><li class="button arrow-right-icon"></li></ul>';
+    var htmlToInsert = [];
+    var entries = result.entries;
+    var l = entries.length;
+    htmlToInsert.push('<div class="sub-result">');
+    htmlToInsert.push('<div class="entries">');
+    for(var i = 0; i < l; ++i) {
+        var e = entries[i];
+        htmlToInsert.push('<div class="entry">');
+        htmlToInsert.push('<div class="info">');
+        htmlToInsert.push('<h3><em>' + e.title + '</em></h3>');
+        if(e.authors) {
+            htmlToInsert.push('<p>' + e.authors + 
+                (e.publication? '.&nbsp' + e.publication : '') + '</p>');
+        }
+        var k = e.tags.length;
+        if(k > 0) {
+            htmlToInsert.push('<p>');
+            for(var j = 0; j < k; ++j) {
+                htmlToInsert.push('<span class="tag">' + e.tags[j] + '</span>');
+            }
+            htmlToInsert.push('</p>');
+        }
+        htmlToInsert.push('</div>');
+        htmlToInsert.push(rightBtnHtml);
+        htmlToInsert.push('</div>'); 
     }
-};
+    htmlToInsert.push('</div></div>');
+    $result.append(htmlToInsert.join('')); 
+    $result.fadeIn();
+    // 
+    this.updateHeight();
+}
+/**************************Web Service**************************************/
+function FmWebService() {
+    this.url = {
+        root: "ws/",
+        docsSearch: "docs/search",
+        docsUpload: "docs/upload",
+        docsDelete: "docs/"
+    };
+}
+FmWebService.prototype.docsSearch = function(tag, keywords, context, callback) {
+    var response = {
+        error: null,
+        result: {
+            sortedBy: "addedOn",
+            total: 15,
+            entries: [
+                {   docId: "1111111",
+                    title: "Zephyr: Live Migration in Shared Nothing Databases for Elastic Cloud Platforms",
+                    authors: "Peter Bakkum, Kevin Skadron", 
+                    publication: "SIGMOD 2011",
+                    year: "2011",
+                    addedOn: "Feb 19 2012",
+                    tags: ["live migration", "SIGMOD'11"] },
+                {   docId: "2222222",
+                    title: "Brighthouse: An Analytic Data Warehouse for Ad-hoc Queries", 
+                    authors: "Dominik Slezak, Jakub Wroblewski, Victoria Eastwood, Piotr Synak", 
+                    publication: "VLDB '09", 
+                    addedOn: "Feb 8 2012",
+                    tags: ["column store", "VLDB'09"] },
+                {   docId: "3333333",
+                    title: "The End of an Architectural Era", 
+                    authors: "Michael Stonebraker, Samuel Madden, Daniel J. Abadi", 
+                    publication: "VLDB '07", 
+                    addedOn: "Jan 6 2012",
+                    tags: ["column store"] } 
+            ]
+        }
+    };
+    setTimeout(function() {
+        callback(response, context);
+    }, 1000);
+}
+FmWebService.prototype.docsUpload = function() {
+}
+FmWebService.prototype.docsDelete = function() {
+}
+FmWebService.prototype.docsDetail = function() {
+}
+FmWebService.prototype.docsEdit = function() {
+}
+FmWebService.prototype.tagsList = function() {
+}
+FmWebService.prototype.tagAdd = function() {
+}
+FmWebService.prototype.tagEdit = function() {
+}
+FmWebService.prototype.tagDelete = function() {
+}
+/******************************Initialization********************************/
 $(document).ready(function() {
     var manager = new FmManager();
     manager.init();
